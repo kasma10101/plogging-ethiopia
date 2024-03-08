2c352988d0f07b5d28861a789c1c01435e1f18e4
adnan4k <fayomuhe5@gmail.com>
2024-03-08 21:25:06 +0300

    setup complete

---
+++ backend/models/admin.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  name: String,
+  email: {type: String, unique: true},
+  phoneNumber: String,
+  password: String,
+  isAdmin:{
+    type:Boolean,
+    default:true
+  }
+});
+
+const adminModel = mongoose.model('admin', adminSchema);
+
+module.exports = adminModel;
